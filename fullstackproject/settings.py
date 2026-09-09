c14db3f1bdd4e27646a1bab73e44a1ebbadc0df5
Liva Mary <rlivamary@gmail.com>
2026-09-09 19:11:34 +0530

    Configure Render allowed hosts

--- fullstackproject/settings.py
+++ fullstackproject/settings.py
@@ -28,9 +28,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = [
+    os.getenv("RENDER_EXTERNAL_HOSTNAME", "127.0.0.1"),
+    "localhost",
+]
 # Application definition
 
 INSTALLED_APPS = [
